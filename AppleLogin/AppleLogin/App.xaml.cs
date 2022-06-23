ecab3115536c048829a626767c7bd8e9e0bf234f
Caio Lima de Almeida <caio.almeida@e-gestao.com.br>
2022-06-23 14:28:10 -0300

    Adicionar arquivos de projeto.

---
+++ AppleLogin/AppleLogin/App.xaml.cs
@@ -0,0 +1,59 @@
+﻿using AppleLogin.Interfaces;
+using System;
+using Xamarin.Forms;
+using AppleLogin.Model;
+using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+
+namespace AppleLogin
+{
+    public partial class App : Application
+    {
+        public const string LoggedInKey = "LoggedIn";
+        public const string AppleUserIdKey = "WNAYSK88JX.com.companyname.BeepIt";
+        string userId;
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override async void OnStart()
+        {
+            var appleSignInService = DependencyService.Get<IAppleSignInService>();
+            if (appleSignInService != null)
+            {
+                userId = await SecureStorage.GetAsync(AppleUserIdKey);
+                if (appleSignInService.IsAvailable && !string.IsNullOrEmpty(userId))
+                {
+
+                    var credentialState = await appleSignInService.GetCredentialStateAsync(userId);
+
+                    switch (credentialState)
+                    {
+                        case AppleSignInCredentialState.Authorized:
+                            //Normal app workflow...
+                            break;
+                        case AppleSignInCredentialState.NotFound:
+                        case AppleSignInCredentialState.Revoked:
+                            //Logout;
+                            SecureStorage.Remove(AppleUserIdKey);
+                            Preferences.Set(LoggedInKey, false);
+                            MainPage = new Logado();
+                            break;
+                    }
+                }
+            }
+
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AppleLogin/AppleLogin/Interfaces/IAppleSignInService.cs
@@ -0,0 +1,15 @@
+﻿using AppleLogin.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppleLogin.Interfaces
+{
+    public interface IAppleSignInService
+    {
+        bool IsAvailable { get; }
+        Task<AppleSignInCredentialState> GetCredentialStateAsync(string userId);
+        Task<AppleAccount> SignInAsync();
+    }
+}
